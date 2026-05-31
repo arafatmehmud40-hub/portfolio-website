89bc3cc9b776ac29ad46b651d3335c6b4e5175f5
arafatmehmud40-hub <arafatmehmud40@gmail.com>
2026-05-31 11:55:41 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Video Editor", "Graphics Designer", "Data Analyst", ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+}); 
+
